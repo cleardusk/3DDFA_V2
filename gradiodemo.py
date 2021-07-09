3ce72440c8d3f4046af684c819928c69dab9e31e
AK391 <81195143+AK391@users.noreply.github.com>
2021-07-09 15:45:05 -0400

    add examples

--- gradiodemo.py
+++ gradiodemo.py
@@ -2,6 +2,9 @@
 import sys
 from subprocess import call
 import os
+import torch
+
+torch.hub.download_url_to_file('https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Solvay_conference_1927.jpg/1400px-Solvay_conference_1927.jpg', 'solvay.jpg')
 
 def run_cmd(command):
     try:
@@ -72,15 +75,18 @@ def inference (img):
     return render(img, ver_lst, tddfa.tri, alpha=0.6, show_flag=False);    
 
 
-title = "3DDFA_V2"
-description = "demo for 3DDFA_V2. To use it, simply upload your image, or click one of the examples to load them. Read more at the links below."
+title = "3DDFA V2"
+description = "demo for 3DDFA V2. To use it, simply upload your image, or click one of the examples to load them. Read more at the links below."
 article = "<p style='text-align: center'><a href='https://arxiv.org/abs/2009.09960'>Towards Fast, Accurate and Stable 3D Dense Face Alignment</a> | <a href='https://github.com/cleardusk/3DDFA_V2'>Github Repo</a></p>"
-
+examples = [
+    ['solvay.jpg']
+]
 gr.Interface(
     inference, 
     [gr.inputs.Image(type="numpy", label="Input")], 
     gr.outputs.Image(type="numpy", label="Output"),
     title=title,
     description=description,
-    article=article
-    ).launch(debug=True)
+    article=article,
+    examples=examples
+    ).launch()
